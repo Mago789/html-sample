af11e64885ea61da57b43a2e5ad99e41012ff8d5
馬篭知也 <mago@magometomoyanoMacBook-Air.local>
2021-04-26 20:34:38 +0900

    add

---
+++ components/sample1.js
@@ -0,0 +1,35 @@
+// 数値は　7　のようにそのまま記述する
+// テキストの値は "こんにちは"　のように""で囲む
+// 真偽値はfalseとtrueの二つのみ
+// 変数宣言はvarとletの二つ
+// varはscript全般
+// letは変数を用意したところだけで使う
+// var 変数名　=　値
+// const 定数　= 値
+// 数値計算は+-*/
+// %にて余りを求められる
+// 比較演算子は==や!=
+// テキストは+を使って他のテキストやそれ以外の値と繋げられる
+// 文の終わりは改行するか;をつけるかで示すが、両方使うのが一般的
+
+const a = 2
+const b = 3
+var c = 10
+if a > 0 {
+    a+b
+} else {
+    a-b
+}
+
+// switc構文についてjlさjf：dkぁsjfヵdjsflk：あsdjf；ぁsdkぁ
+
+while c > 0 {
+    c = c - b
+}
+
+// forについてjsあ、fkぁsdjfkぁsjfさkfsd
+
+// 配列の書き方{a, b, c}
+var line1 = new Array()
+var line2 = new Array(要素の数)
+var line3 = [値]
