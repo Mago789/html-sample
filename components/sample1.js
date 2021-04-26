f6968f28a8e3a7da3a6d891f2814630b1a77a661
Mago789 <kanmago0708@icloud.com>
2021-04-27 01:32:05 +0900

    add js-code

--- components/sample1.js
+++ components/sample1.js
@@ -28,8 +28,17 @@ while c > 0 {
 }
 
 // forについてjsあ、fkぁsdjfkぁsjfさkfsd
+// for (初期化 ;  条件; 後処理) {} //繰り返し
+// for (変数 in 配列)
 
 // 配列の書き方{a, b, c}
 var line1 = new Array()
 var line2 = new Array(要素の数)
 var line3 = [値]
+// 配列の変数名　[番号] = 値
+// 変数　= 配列[番号]
+
+// function 関数 (引数) {} //関数を作る時のキーワード
+
+// var 変数 = function (引数) {}
+// function 関数 (引数) {   return 値}
