19e3100d0d1e68e4ea645c7fba8fff8c992a9862
Mago789 <kanmago0708@icloud.com>
2021-04-28 02:17:38 +0900

    add if

--- components/js-sample.js
+++ components/js-sample.js
@@ -38,3 +38,12 @@ a < 0
 // | | = または
 // xが2の時 x < 1 || x < 3 複数の条件のうち片方が真
 
+if1 = 5
+if (if1 > 0){ // 条件式がtrueの時実行
+    if1 = if1
+} else if (if1 < 0 && if1 > -2){ // 条件1がfalse,条件2がtrue
+    if1 = if1 * 2
+} else { // 条件1,2がfalse
+    if1 = -if1
+}
+
