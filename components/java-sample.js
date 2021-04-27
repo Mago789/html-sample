1921d27b341333f80f67700e592654f9715bd999
Mago789 <kanmago0708@icloud.com>
2021-04-27 22:24:27 +0900

    add new file.js

---
+++ components/java-sample.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom"
+
+class Layout extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello</h1>
+                <h1>It works!</h1>
+            </div>
+        )
+    }
+}
+
+const app = document.getElementById("app");
+ReactDOM.render(<Layout/>, app);
