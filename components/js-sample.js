6942205bbf00c8036fe985776a629d6024ddf559
Mago789 <kanmago0708@icloud.com>
2021-04-28 01:30:38 +0900

    add log

---
+++ components/js-sample.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+alert("Hello World!!");
+
+console.log(5 + 3);
+console.log(5 - 3);
+console.log(4 * 2);
+console.log(4 / 2);
+console.log(9 / 5);
+console.log("5 + 3");
+console.log("5" + "3");
+console.log("ABC" + "DEF");
+
