fd19e8807d837057d62727e5bd5a5b5cc5172f7f
Mago789 <kanmago0708@icloud.com>
2021-04-28 01:58:36 +0900

    add kiso

--- components/js-sample.js
+++ components/js-sample.js
@@ -11,3 +11,30 @@ console.log("5 + 3");
 console.log("5" + "3");
 console.log("ABC" + "DEF");
 
+var name = "tanaka";
+console.log(name);
+
+var name ="tanaka"
+console.log(name + tarou);
+
+var x =1 + 100;
+alert(x);
+
+x = 2;
+alert(x);
+
+x = x + 2;
+alert(x);
+
+//x = x + 1 ... x += 1 省略した記述方法　他の四則演算も可能
+
+a === b //aとbが等しい
+a !== b //aとbが等しくない
+a < 0
+
+// && = かつ
+// xが2の時 x > 1 && x < 3 複数の条件が全て真
+
+// | | = または
+// xが2の時 x < 1 || x < 3 複数の条件のうち片方が真
+
