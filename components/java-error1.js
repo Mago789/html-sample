7b1dedde0d8493751927d5cbe4566af0c2fa69b2
Mago789 <kanmago0708@icloud.com>
2021-04-28 01:20:06 +0900

    change name

---
+++ components/java-error1.js
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+import tw from "twin.macro";
+export default () =>(
+    <div>
+        <section
+            style={{
+                backgroundImage:
+                    ""
+            }}
+    </div>
+)
